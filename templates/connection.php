991e4cc00754c5f70e3931276a1c02a913ef39b6
Andy Ball <932400399@bun2-206-11.local>
2019-10-14 09:08:21 +1300

    added comment

--- templates/connection.php
+++ templates/connection.php
@@ -1,5 +1,6 @@
 <?php
 	date_default_timezone_set('Pacific/Auckland');	// vagrant servers might use a different timezone?
+	// we're always going to go -> host, username, password, table, IN THAT ORDER, when setting up dbc 
 	$dbc = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_TABLE'));
 
 	if ($dbc) {
